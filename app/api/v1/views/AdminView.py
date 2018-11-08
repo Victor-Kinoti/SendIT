b81576258a1468283908bb7a905d3b1ad3d0a9b8
Victor-Kinoti <vickeynote@gmail.com>
2018-11-08 23:52:06 +0300

    [Start #161804910] admin update payment status view

--- app/api/v1/views/AdminView.py
+++ app/api/v1/views/AdminView.py
@@ -8,3 +8,10 @@ class admin_update_order_status(Resource):
 		order_1 = UserOrders()
 		order_1.update_order_status(user_id)
 		return make_response(jsonify({'message': 'order has been delivered!'}),200)
+
+class admin_update_payment_status(Resource):
+	def put(self, user_id):
+		user_id = str(user_id)
+		order_1 = UserOrders()
+		order_1.update_order_payment(user_id)
+		return make_response(jsonify({"message": "order paid!"}),200)
