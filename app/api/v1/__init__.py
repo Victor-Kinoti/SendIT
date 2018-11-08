c47442f305fdf675075591cab01a5e0102d36c79
Victor-Kinoti <vickeynote@gmail.com>
2018-11-08 22:37:30 +0300

    [Finishes #161805209] url to access single orer

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
-from .views.UserView import DataParcel
+from .views.UserView import DataParcel, SingleParcel
 
 version1 = Blueprint('v1', __name__,  url_prefix = '/api/v1')
 
@@ -9,3 +9,4 @@ api = Api(version1)
 
 ##user routes
 api.add_resource(DataParcel, '/parcels')
+api.add_resource(SingleParcel, '/parcels/<order_id>')
