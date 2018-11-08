b79732b4c676f3bef8226affaf613dfc8dbadd5b
Victor-Kinoti <vickeynote@gmail.com>
2018-11-08 21:47:25 +0300

    [Finishes #161713254] update urls in init file]

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
+from .views.UserView import DataParcel
+
 version1 = Blueprint('v1', __name__,  url_prefix = '/api/v1')
 
 api = Api(version1)
+
+
+##user routes
+api.add_resource(DataParcel, '/parcels')
