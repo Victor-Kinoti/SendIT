26e981208b79d043663c39178e90052e8adfcb4f
Victor-Kinoti <vickeynote@gmail.com>
2018-11-08 23:20:02 +0300

    [Start #161804965] url to access view UPDATE/PUT state

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 from .views.UserView import DataParcel, SingleParcel, CancelOrder
+from .views.AdminView import  admin_update_order_status
 
 version1 = Blueprint('v1', __name__,  url_prefix = '/api/v1')
 
@@ -11,3 +12,5 @@ api = Api(version1)
 api.add_resource(DataParcel, '/parcels')
 api.add_resource(SingleParcel, '/parcels/<order_id>')
 api.add_resource(CancelOrder, '/parcels/<order_id>/cancel')
+
+api.add_resource(admin_update_order_status, 'users/<user_id>/delivered')
