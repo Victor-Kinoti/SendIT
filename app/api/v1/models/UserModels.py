6cf8d93294b112a3b5701a4a034c76be61afd58b
Victor-Kinoti <vickeynote@gmail.com>
2018-11-08 21:26:31 +0300

    [Start #161713254]created a usermodel file for all user models. added post method logic]

---
+++ app/api/v1/models/UserModels.py
@@ -0,0 +1,26 @@
+import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Parcel(object):
+	parcels = []
+	def create_order(self,pickup_addr,destination_addr,item_type,recipient_name,recipient_id,weight,status):
+		self.pickup_addr = pickup_addr
+		self.destination_addr = destination_addr
+		self.recipient_name = recipient_name
+		self.recipient_id = recipient_id
+		self.item_type = item_type
+		self.weight = weight
+		self.status = status
+
+		payload  ={
+		"order_id": str(uuid.uuid4().int),
+		"pickup_address":self.pickup_addr,
+		"destination_address":self.destination_addr,
+		"recipient_name":self.recipient_name,
+		"recipient_id":self.recipient_id,
+		"item_type":self.item_type,
+		"status":self.status
+		}
+
+		Parcel.parcels.append(payload)
+		return True
