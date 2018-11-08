0531f44094a6c15b7e560d27abc6b777ad5046ca
Victor-Kinoti <vickeynote@gmail.com>
2018-11-08 23:57:34 +0300

    [Finishes #161804910] add url loads fuction call on models for PUT method

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 from .views.UserView import DataParcel, SingleParcel, CancelOrder
-from .views.AdminView import  admin_update_order_status
+from .views.AdminView import  admin_update_order_status,admin_update_payment_status
 
 version1 = Blueprint('v1', __name__,  url_prefix = '/api/v1')
 
@@ -12,5 +12,6 @@ api = Api(version1)
 api.add_resource(DataParcel, '/parcels')
 api.add_resource(SingleParcel, '/parcels/<order_id>')
 api.add_resource(CancelOrder, '/parcels/<order_id>/cancel')
-
+##admin routes
+api.add_resource(admin_update_payment_status, 'users/<user_id>/paid')
 api.add_resource(admin_update_order_status, 'users/<user_id>/delivered')
