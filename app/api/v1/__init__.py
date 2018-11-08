fca2e7a03719bbe8533372b56c49a0a4988a4dd1
Victor-Kinoti <vickeynote@gmail.com>
2018-11-08 22:54:52 +0300

    [Finishes #161805144] url to route to cancel orders

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
-from .views.UserView import DataParcel, SingleParcel
+from .views.UserView import DataParcel, SingleParcel, CancelOrder
 
 version1 = Blueprint('v1', __name__,  url_prefix = '/api/v1')
 
@@ -10,3 +10,4 @@ api = Api(version1)
 ##user routes
 api.add_resource(DataParcel, '/parcels')
 api.add_resource(SingleParcel, '/parcels/<order_id>')
+api.add_resource(CancelOrder, '/parcels/<order_id>/cancel')
