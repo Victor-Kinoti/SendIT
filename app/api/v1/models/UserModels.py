e3016017f453f93e3ac98cc330c0c22f4b6373ed
Victor-Kinoti <vickeynote@gmail.com>
2018-11-09 05:14:56 +0300

    [Start #161804633] login model/logic for POST method

--- app/api/v1/models/UserModels.py
+++ app/api/v1/models/UserModels.py
@@ -44,3 +44,38 @@ class Parcel(object):
 			if item["order_id"] == order_id:
 				item['status'] = 'canceled'
 				return True
+
+class User_model(object):
+	fields = []
+	def create_user(self, email, username, password, con_password, role):
+		self.email = email
+		self.username = username 
+		self.password = password
+		self.con_password = con_password
+		self.role = role
+
+
+		payload={
+			"user_id": str(uuid.uuid4().int),
+			"email":self.email,
+			"username":self.username,
+			"password":self.password,
+			"con_password":self.con_password,
+			"role":self.role
+		}
+		User_model.fields.append(payload)
+		return True
+
+	def login_user(self, email, password, role):
+		self.email = email
+		self.password = password,
+		self.role = role
+
+		payload={
+			"user_id":str(uuid.uuid4().int),
+			"email":self.email,
+			"password":self.password,
+			"role":self.role
+		}
+		User_model.fields.append(payload)
+		return True
