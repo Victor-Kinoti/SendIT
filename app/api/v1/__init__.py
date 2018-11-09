6ec253a4ec19bdec407f693b1dde750477297b83
Victor-Kinoti <vickeynote@gmail.com>
2018-11-09 04:56:03 +0300

    fixing import errors

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,12 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
-from .views.UserView import DataParcel, SingleParcel, CancelOrder
+from .views.UserViews import DataParcel, SingleParcel, CancelOrder
 from .views.AdminView import  admin_update_order_status,admin_update_payment_status
 
 version1 = Blueprint('v1', __name__,  url_prefix = '/api/v1')
 
 api = Api(version1)
-
 
 ##user routes
 api.add_resource(DataParcel, '/parcels')
