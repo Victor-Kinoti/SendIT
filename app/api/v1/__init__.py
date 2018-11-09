030926cb91518b91e436ffd2341073e4a7beb45d
Victor-Kinoti <vickeynote@gmail.com>
2018-11-09 05:21:47 +0300

    [Finishes #161804633] include url for login in init with blueprints

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
-from .views.UserView import DataParcel, SingleParcel, CancelOrder
+from .views.UserViews import DataParcel, SingleParcel, CancelOrder, UserLogin
 from .views.AdminView import  admin_update_order_status,admin_update_payment_status
 
 version1 = Blueprint('v1', __name__,  url_prefix = '/api/v1')
@@ -12,6 +12,7 @@ api = Api(version1)
 api.add_resource(DataParcel, '/parcels')
 api.add_resource(SingleParcel, '/parcels/<order_id>')
 api.add_resource(CancelOrder, '/parcels/<order_id>/cancel')
+api.add_resource(UserLogin, '/login')
 ##admin routes
 api.add_resource(admin_update_payment_status, 'users/<user_id>/paid')
 api.add_resource(admin_update_order_status, 'users/<user_id>/delivered')
