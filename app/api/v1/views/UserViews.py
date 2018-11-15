223bc38ff62370343ee0ccba8d838d2afd54dfa6
Victor-Kinoti <vickeynote@gmail.com>
2018-11-15 21:29:28 +0300

    email and username exist edge cases

--- app/api/v1/views/UserViews.py
+++ app/api/v1/views/UserViews.py
@@ -8,6 +8,7 @@ class DataParcel(Resource):
 	"""Utilizes data from an order by either getting all 
 	data or posting new data"""
 	def post(self):
+		"""Create an order"""
 		data = request.get_json() or {}
 
 		if 'destination_address' not in data or 'pickup_address'\
@@ -55,6 +56,7 @@ class DataParcel(Resource):
 		return result
 		
 class SingleParcel(Resource):
+	"""gets a single order"""
 	def get(self, order_id):
 
 		try:
@@ -79,6 +81,7 @@ class SingleParcel(Resource):
 		
 class CancelOrder(Resource):
 	def put(self, order_id):
+		"""cancels an order that exists"""
 		data = request.get_json()
 		try:
 			order_id = int(order_id)
@@ -87,19 +90,19 @@ class CancelOrder(Resource):
 				"Message":"Provide a valid order id",
 				"Status": "bad request"
 			}),400)
-		if order_id is not None:
-			order_id = str(order_id)
-			print(data['order_id'])
-			if order_id == data['order_id']:
-				order_1 = Order()
-				order_1.cancel_order(order_id)
-				return make_response(jsonify({'Status': \
+		# if order_id is not None:
+		par=Order()
+		order=par.get_one_order(order_id)
+		if order:
+			order['order_status'] == 'Canceled'
+			return make_response(jsonify({'Status': \
 				'order has been canceled'}),201)
 			
 		return make_response(jsonify({"Status": \
 		"Order doesn't exist"}),400)
 
 class RegisterUser(Resource):
+	"""This class creates a new user"""
 	def post(self):		
 			
 		data = request.get_json() or {}
@@ -122,12 +125,20 @@ class RegisterUser(Resource):
 			abort(make_response(jsonify(message=\
 			"wrong email format"),400))
 
-			
 		if len(data)==0:
 			abort(make_response(jsonify(message=\
 			"Fill in the fields"),400))
 		
 		user_1 = User_model()
+		for item in User_model.fields:
+			if data['email'] == item['email']:
+				abort(make_response(jsonify(message=\
+			"email already exists"),400))
+
+			if data['username'] == item['username']:
+				abort(make_response(jsonify(message=\
+			"username already exists"),400))				
+
 		user_1.create_user(data)
 
 		payload = {
@@ -139,6 +150,7 @@ class RegisterUser(Resource):
 		return result
 
 class UserLogin(Resource):
+	"""Logs in an existing user"""
 	def post(self):
 		data = request.get_json()
 		if 'email' not in data:
